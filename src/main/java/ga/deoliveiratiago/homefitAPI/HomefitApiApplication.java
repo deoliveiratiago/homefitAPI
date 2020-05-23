a9da4005566349e5111c5b61f700a1656af27650
Tiago <deoliveiratiago@outlook.com>
2020-05-23 11:11:34 -0300

    Projeto criado

---
+++ src/main/java/ga/deoliveiratiago/homefitAPI/HomefitApiApplication.java
@@ -0,0 +1,13 @@
+package ga.deoliveiratiago.homefitAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomefitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomefitApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ga/deoliveiratiago/homefitAPI/HomefitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.deoliveiratiago.homefitAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomefitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
